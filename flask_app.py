b6c25af2b19d70b6473c700dc51214041c29096d
Wes Chow <wgesh8@gmail.com>
2020-01-11 19:52:34 -0800

    Add firestore function

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,21 @@
 from flask import Flask, request, render_template, send_from_directory
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Initialize app with environment variable
+default_app = firebase_admin.initialize_app()
+
+# Setup services
+db = firestore.client()
+
+# Test adding data to database
+# doc_ref = db.collection(u'users').document(u'wchow').collection(u'testing').document(u'notes' + num)
+# doc_ref.set({
+#     u'first': u'Wes',
+#     u'last': u'Chow',
+#     u'born': 2003
+# })
 
 app = Flask(__name__)
 
