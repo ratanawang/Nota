05983b252d3e2cd40db5470c5be7c43577995cc2
Ratana <ratanawang8@gmail.com>
2020-01-11 18:34:53 -0800

    set up Google natural language API

---
+++ google_credentials.py
@@ -0,0 +1,24 @@
+
+def implicit():
+    from google.cloud import storage
+
+    # If you don't specify credentials when constructing the client, the
+    # client library will look for credentials in the environment.
+    storage_client = storage.Client()
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
+
+
+def explicit():
+    from google.cloud import storage
+
+    # Explicitly use service account credentials by specifying the private key
+    # file.
+    storage_client = storage.Client.from_service_account_json(
+        '/Users/ratanawang/Desktop/Nota-c458ee949d32.json')
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
