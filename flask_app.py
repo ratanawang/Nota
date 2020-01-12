567bec4bbae7e8d22b1e5ad393d8350cbae990f7
Wes Chow <wgesh8@gmail.com>
2020-01-12 11:53:22 -0800

    Connect frontend and backend login

--- flask_app.py
+++ flask_app.py
@@ -1,18 +1,20 @@
-from flask import Flask, request, render_template, send_from_directory, flash
+from flask import Flask, request, render_template, send_from_directory, flash, redirect
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase_admin import auth
-from firebase_admin import storage
+# from firebase_admin import storage
 
 # Initialize app with environment variable
 default_app = firebase_admin.initialize_app()
+
+user_logged_in = False
 
 # Setup services
 db = firestore.client()
 auth = firebase_admin.auth
 
-# Test adding data to database
+    # Test adding data to database
 # doc_ref = db.collection(u'users').document(u'wchow')
 # doc_ref.set({
 #     u'first': u'Wes',
@@ -20,60 +22,60 @@ auth = firebase_admin.auth
 #     u'born': 2003
 # })
 
-# Test user authentication stuff
-# input register
-email = input("(create acc) enter email: ")
-password = input("(create acc) enter password: ")
-sample_data = input("enter some sample data: ")
+    # Test user authentication stuff
+    # input register
+# email = input("(create acc) enter email: ")
+# password = input("(create acc) enter password: ")
+# sample_data = input("enter some sample data: ")
 
-# create user and add data to Firestore
-try:
-    user = auth.create_user(email=email, password=password, disabled=False)
-except Exception:
-    print(u'Account already exists')
-print(user.uid)
-data = {
-    u'uid': user.uid,
-    u'password': password,
-    u'note count': 0
-}
-db.collection(u'users').document(user.uid).set(data)
+    # create user and add data to Firestore
+# try:
+#     user = auth.create_user(email=email, password=password, disabled=False)
+# except Exception:
+#     print(u'Account already exists')
+# print(user.uid)
+# data = {
+#     u'uid': user.uid,
+#     u'password': password,
+#     u'note count': 0
+# }
+# db.collection(u'users').document(user.uid).set(data)
 
-data = {
-    u'sample_data': sample_data
-}
-db.collection(u'users').document(user.uid).collection(u'sample_data').document(u'set').set(data)
+# data = {
+#     u'sample_data': sample_data
+# }
+# db.collection(u'users').document(user.uid).collection(u'sample_data').document(u'set').set(data)
 
-# clear variables
-email = ""
-password = ""
-sample_data = ""
+    # clear variables
+# email = ""
+# password = ""
+# sample_data = ""
 
-# input sign in
-input_email = input("(sign in) enter email: ")
-input_password = input("(sign in) enter password: ")
+    # input sign in
+# input_email = input("(sign in) enter email: ")
+# input_password = input("(sign in) enter password: ")
 
-# check user, password and print sample text
-try:
-    user = auth.get_user_by_email(input_email)
-except Exception:
-    print("error with inputted email")
+    # check user, password and print sample text
+# try:
+#     user = auth.get_user_by_email(input_email)
+# except Exception:
+#     print("error with inputted email")
 
-# get document, convert to dict and get password
-doc_ref = db.collection(u'users').document(user.uid).get()
-doc = doc_ref.to_dict()
-get_password = doc["password"]
-
-if input_password == get_password:
-    print(user.uid)
-    doc_ref = db.collection(u'users').document(user.uid)
-    try:
-        doc = doc_ref.get()
-        print(u'Document data: {}'.format(doc.to_dict()))
-    except Exception:
-        print(u'No document found')
-else:
-    print('wrong password')
+    # get document, convert to dict and get password
+# doc_ref = db.collection(u'users').document(user.uid).get()
+# doc = doc_ref.to_dict()
+# get_password = doc["password"]
+#
+# if input_password == get_password:
+#     print(user.uid)
+#     doc_ref = db.collection(u'users').document(user.uid)
+#     try:
+#         doc = doc_ref.get()
+#         print(u'Document data: {}'.format(doc.to_dict()))
+#     except Exception:
+#         print(u'No document found')
+# else:
+#     print('wrong password')
 
 app = Flask(__name__)
 
@@ -113,14 +115,79 @@ def send_scss(path):
     return send_from_directory('scss', path)
 
 
-# @app.route('/login', methods=['POST'])
-# def submit():
-#     form = ReusableForm(request.form)
-#     if request.method == 'POST':
-#         input_email1 = request.form['email']
-#         input_password1 = request.form['password']
-#         print (input_email1, " ", input_password1)
-#         flash('Thanks for registration')
+@app.route('/login', methods=['POST', 'GET'])
+def submit():
+    global user_logged_in
+    if request.form['action'] == 'Sign In':
+        # get email and password from login/register form
+        input_email = request.form['email']
+        input_password = request.form['password']
+
+        # check user and password
+        try:
+            user = auth.get_user_by_email(input_email)
+        except firebase_admin.exceptions.NotFoundError:
+            print("Account not found, sign up instead!")
+        except Exception:
+            print("General error")
+            return render_template('login.html')
+        else:
+            # get document, convert to dict and get password
+            doc_ref = db.collection(u'users').document(user.uid).get()
+            doc = doc_ref.to_dict()
+            get_password = doc["password"]
+
+            if input_password == get_password:
+                print('Signed in')
+                user_logged_in = True
+                return redirect('/')
+            else:
+                print('wrong password')
+                return render_template('login.html')
+
+    elif request.form['action'] == 'Sign Up':
+        # get email and password from login/register form
+        input_email = request.form['email']
+        input_password = request.form['password']
+
+        # check user and password
+        try:
+            user = auth.get_user_by_email(input_email)
+        except firebase_admin.exceptions.NotFoundError:
+            # setup account stuff
+            try:
+                user = auth.create_user(email=input_email, password=input_password, disabled=False)
+            except Exception:
+                print('General error')
+                return render_template('login.html')
+            else:
+                print('Account created successfully')
+                # get document, convert to dict and get password
+                data = {
+                    u'uid': user.uid,
+                    u'password': input_password,
+                    u'note count': 0
+                }
+                db.collection(u'users').document(user.uid).set(data)
+                user_logged_in = True
+                return redirect('/')
+        except Exception:
+            print("General error")
+            return render_template('login.html')
+        else:
+            # get document, convert to dict and get password
+            doc_ref = db.collection(u'users').document(user.uid).get()
+            doc = doc_ref.to_dict()
+            get_password = doc["password"]
+
+            if input_password == get_password:
+                user_logged_in = True
+                return redirect('/')
+            else:
+                print('wrong password')
+                return render_template('login.html')
+    else:
+        print('ERROR')
 
 
 if __name__ == "main":
